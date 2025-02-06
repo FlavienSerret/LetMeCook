c3f8298313ef66b518103ae3d5431cc72a7f2fe4
Benjamin Sagnes <brnjaminsagnes33@gmail.com>
2025-02-06 09:35:55 +0100

    création navbar

---
+++ view/php/navbar.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-dark">
+    <!-- Logo -->
+    <a href="index.php" class="navbar-brand">Let me Cook</a>
+
+    <!-- Favoris + Search Bar -->
+    <div class="d-flex">
+        <!-- Bouton Favoris -->
+        <a href="favoris.php" class="btn btn-warning me-3">
+            ⭐ Favoris
+        </a>
+
+        <!-- Barre de recherche -->
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Rechercher..." aria-label="Search">
+        </form>
+    </div>
+
+    <!-- Bouton Connexion -->
+    <a href="connexion.php" class="btn btn-warning">Connexion</a>
+</nav>
